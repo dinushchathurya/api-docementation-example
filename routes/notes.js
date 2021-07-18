ff9442f261b2e4f84e00b7b4d079063f0088a1fe
dinushchathurya <dinushchathurya21@gmail.com>
2021-07-18 17:38:44 +0530

    :sparkles: feat: add controller functions to routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,18 @@
 module.exports = (app) => {
     const notes = require('../controllers/note');
 
+    // Create a new Note
+    app.post('/notes', notes.createNote);
+
     // Retrieve all Notes
     app.get('/notes', notes.getAllNotes);
+
+    // Retrieve a single Note with noteId
+    app.get('/notes/:id', notes.getSingleNote)
+
+    // Update a Note with noteId
+    app.put('/notes/:id', notes.updateSingleNote);
+
+    // Delete a Note with noteId
+    app.delete('/notes/:id', notes.deleteSingleNote);
 }
